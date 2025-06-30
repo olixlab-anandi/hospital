2d7a35e4c71e7cc70a364a34684c61733c6da363
olixlab-anandi <olixlab.50@gmail.com>
2025-06-30 10:25:57 +0530

    'changes'

--- src/app/api/patient/[id]/route.ts
+++ src/app/api/patient/[id]/route.ts
@@ -1,9 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import AuthCheck from "@/middleware/AuthCheck";
 import connection from "@/DB/connection";
 import patientModel from "../../../../../model/patientSchema";
 
-export async function GET(req: Request, context: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
     const { id } = context.params;
     if (!id) {
@@ -14,18 +17,14 @@ export async function GET(req: Request, context: { params: { id: string } }) {
     }
     await connection();
     const token = req.headers.get("authorization");
-    console.log("Token received:", token);
+
     const res = await AuthCheck(token as string);
-    console.log(
-      "Auth Check Result:======================================================",
-      res
-    );
+
     if (
       typeof res == "object" &&
       "role" in res &&
       (res?.role == "admin" || res?.role == "staff")
     ) {
-      console.log("Fetching patient details for ID:", id);
       const patient = await patientModel.findById(id);
       return NextResponse.json(patient);
     } else {
